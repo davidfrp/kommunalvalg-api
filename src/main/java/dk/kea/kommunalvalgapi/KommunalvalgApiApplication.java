dc2e030d45f70e7025bd32543b65bfc191379e0a
davidfrp <davidfrp@outlook.dk>
2021-12-20 10:14:31 +0100

    Initial commit

---
+++ src/main/java/dk/kea/kommunalvalgapi/KommunalvalgApiApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.kommunalvalgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommunalvalgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KommunalvalgApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/kommunalvalgapi/KommunalvalgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.kommunalvalgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunalvalgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
