435d70f82b24431ff5d0528e28d0ca00d33c4666
davidfrp <davidfrp@outlook.dk>
2021-12-20 14:26:53 +0100

    Updated files and added dummy data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO parties (signature, abbreviation, name, hex_color) VALUES
+    ('A', 'S', 'Socialdemokratiet', '#ff0000'),
+    ('V', 'V', 'Venstre', '#0000ff'),
+    ('O', 'DF', 'Dansk Folkeparti', '#00ffff'),
+    ('F', 'SF', 'Socialistisk Folkeparti', '#ff0000'),
+    ('B', 'RV', 'Radikale Venstre', '#ff00ff'),
+    ('Ø', 'EL', 'Enhedslisten', '#ff0000'),
+    ('C', 'KF', 'Det Konservative Folkeparti', '#ff0000'),
+    ('D', 'NB', 'Nye Borgerlige', '#00ff00'),
+    ('I', 'LA', 'Liberal Alliance', '#0000ff'),
+    ('Q', 'FG', 'Frie Grønne', '#00ff00'),
+    ('K', 'KD', 'Kristendemokraterne', '#ff00ff');
